f2584b0f267abc2de77558a771107cffcf6e12ce
brian <brian@mutualaid.info>
2025-01-18 16:57:02 -0500

    Add more data

--- cidr_bot/bot.py
+++ cidr_bot/bot.py
@@ -34,6 +34,41 @@ class CidrBot(Plugin):
             message_lines.append(f"- Prefix: `{network.prefixlen}`")
             message_lines.append(f"- Netmask: `{network.netmask.exploded}`")
             message_lines.append(f"- Broadcast address: `{network.broadcast_address.exploded}`")
+            message_lines.append(f"- PTR record name: `{network.reverse_pointer}`")
+            if network.is_multicast:
+                message_lines.append(
+                    "- Address is reserved for multicast use per "
+                    "[RFC 3171](https://datatracker.ietf.org/doc/html/rfc3171.html)."
+                )
+            if network.is_private:
+                message_lines.append(
+                    "- Address is private (not globally reachable) per the "
+                    "[IANA IPv4 Special-Purpose Address Registry]"
+                    "(https://www.iana.org/assignments/iana-ipv4-special-registry/"
+                    "iana-ipv4-special-registry.xhtml)."
+                )
+            elif network.is_global:
+                message_lines.append(
+                    "- Address is globally reachable per the "
+                    "[IANA IPv4 Special-Purpose Address Registry]"
+                    "(https://www.iana.org/assignments/iana-ipv4-special-registry/"
+                    "iana-ipv4-special-registry.xhtml)."
+                )
+            else:
+                message_lines.append(
+                    "- Address is designated for Carrier-Grade NAT (CGN) per "
+                    "[RFC 6598](https://datatracker.ietf.org/doc/html/rfc6598)."
+                )
+            if network.is_loopback:
+                message_lines.append(
+                    "- Address is a loopback address per "
+                    "[RFC 3330](https://datatracker.ietf.org/doc/html/rfc3330.html)."
+                )
+            if network.is_link_local:
+                message_lines.append(
+                    "- Address is reserved for link-local usage per "
+                    "[RFC 3927](https://datatracker.ietf.org/doc/html/rfc3927.html)."
+                )
             await evt.reply("\n".join(message_lines))
         except ValueError:
             await evt.reply("Invalid address.")
